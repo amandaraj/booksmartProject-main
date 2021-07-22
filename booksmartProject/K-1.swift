d5e78f4edf56ef68678e54efa9362c04d9473db6
group 5 <mtqchen@gmail.com>
2021-07-22 12:03:33 -0500

    * added images/audiobooks, did some design, added segues, working on links

--- booksmartProject/K-1.swift
+++ booksmartProject/K-1.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class K_1: UIViewController {
 
+    @IBAction func giveamouseacookie(_ sender: Any) {
+        if let url = URL(string: "https://www.youtube.com/watch?v=bzw0pDLAf7k") {
+              UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    @IBAction func peterrabbit(_ sender: Any) {
+    }
+    @IBAction func sylvestermagicpebble(_ sender: Any) {
+    }
+    @IBAction func theemptypot(_ sender: Any) {
+    }
+    @IBAction func hanukkahinalaska(_ sender: Any) {
+    }
+    @IBAction func clarktheshark(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
